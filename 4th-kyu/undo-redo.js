f0ad1b1a080d4be7644a92447d025adf797019ba
Christina Milner <christina@christina-milner.dev>
2022-10-29 21:08:37 +0100

    Thought I had the 4th kyu figured out, but no

--- 4th-kyu/undo-redo.js
+++ 4th-kyu/undo-redo.js
@@ -77,3 +77,64 @@ function undoRedo(object) {
 
 // WIP - the undo and redo functions do not work (apart from the horrible code duplication), but I bit off a little more than I could chew here
 // and will have to revisit this another time.
+
+// Just had to revisit this - thought maybe if I just copied the previous object rather than trying to painstakingly keep track of the 
+// modified keys and values with each step, it'd work. (At that point, you could really use the same function for undo and redo, but anyway.)
+
+function undoRedo(object) {
+	return {
+    operations: [],
+    prevObjects: [],
+    makeBackUp: function() {
+      let previous = {}
+      Object.assign(previous, object)
+      this.prevObjects.push(previous)
+    },
+		set: function(key, value) {
+      this.makeBackUp()
+      object[key] = value
+      this.operations.push("set")
+    },
+		get: function(key) {
+      return object[key]
+    },
+		del: function(key) {
+      this.makeBackUp()
+      this.operations.push("delete")
+      delete object[key]
+    },
+		undo: function() {
+      if (!this.operations.length || this.operations.every (e => e == "undo")) {
+        throw new Error()
+      }
+      else {
+
+        let temp = {}
+        Object.assign(temp, this.prevObjects.pop())
+        this.makeBackUp()
+        object = {}
+        Object.assign(object, temp)
+        this.operations.pop()
+        this.operations.push("undo")
+      }
+    },
+		redo: function() {
+            if (this.operations.slice(-1)[0] !== "undo") {
+                throw "No redo here"
+            }
+            else {
+              let temp = {}
+              Object.assign(temp, this.prevObjects.pop())
+              this.makeBackUp()
+              object = {}
+              Object.assign(object, temp)
+              this.operations.pop()
+              this.operations.push("redo")
+            }
+        }
+  }
+}
+
+// This passes all of the fixed tests except one, one that uses multiple undos in a row. Also fails several randoms, likely for the same reason.
+// That reason continues to elude me, but brain's in a knot and I've already spent far too much time on this. 
+// Also, the amount of time I spent wondering why the exception blocks were triggering when they weren't supposed to until I realised the slice returns ["undo"] rather than "undo" smh
