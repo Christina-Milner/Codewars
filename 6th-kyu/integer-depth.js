8bd4f9692763ccfaa5081cc7c215c6879909f9ea
Christina Milner <christina@christina-milner.dev>
2022-12-10 18:23:18 +0000

    Fixed issue that wasn't caught by CW tests

--- 6th-kyu/integer-depth.js
+++ 6th-kyu/integer-depth.js
@@ -10,20 +10,37 @@ We convert n to a string, split it, and make a set from that to get the unique d
 Then a while loop that multiplies n with an incrementing i runs until the size of that set is 10. 
 Then return i. */
 
-function computeDepth(x) {
-    let digits = new Set(String(x).split(''))
-    let i = 2
-    while (true) {
-      for (let digit of String(x * i).split('')) {
-        digits.add(digit)
-      }
-      if (digits.size == 10) {break}
-      i++
-    }
-    return i
-}
+// function computeDepth(x) {
+//     let digits = new Set(String(x).split(''))
+//     let i = 2
+//     while (true) {
+//       for (let digit of String(x * i).split('')) {
+//         digits.add(digit)
+//       }
+//       if (digits.size == 10) {break}
+//       i++
+//     }
+//     return i
+// }
 
 /* Had to change it to a while true loop (or, well, decided to fix the issue by doing so, anyway) as otherwise i was getting incremented
 on the round that completed the set and everything was off by 1. Thought I could fix it by doing ++i instead of i++, but apparently not so (I have
 yet to understand what that's for). Adding a check for digits.size to a while loop based on its size seemed
 clumsy and subtracting one at the end like a workaround for faulty logic, so here we are. */
+
+/* UPDATE: It occurred to me after submitting this that it would incorrectly produce 2 in cases where the correct answer was 1, e.g. if given
+1234567890 as input, which Codewars tests didn't account for (issue submitted). Hence above version commented out and following refactored
+solution submitted: */
+
+function computeDepth(x) {
+  let digits = new Set()
+  let i = 1
+  while (true) {
+    for (let digit of String(x * i).split('')) {
+      digits.add(digit)
+    }
+    if (digits.size == 10) {break}
+    i++
+  }
+  return i
+}
