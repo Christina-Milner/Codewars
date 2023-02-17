c29e617d973b0fcf7cc47c8b8b453cd4fb55dd5d
Christina Milner <christina@christina-milner.dev>
2023-02-17 15:10:13 +0000

    Simple fun 396 WIP

---
+++ 6th-kyu/simple-fun-396-longest-substring-unique.js
@@ -0,0 +1,29 @@
+/* Given a string, return the length of the longest substring consisting of unique characters, paying attention to performance. */
+
+//P: A string
+//R: A number
+
+/*
+- Declare max (0) and substring ("")
+- Iterate over string, adding each letter to substring if not present yet
+- If letter already present, check if substring length is greater than max, and if so, update max to it, then reset substring
+*/
+
+function longestSubstringOf(string) {
+	let substr = ""
+    let max = 0
+    for (let letter of string) {
+        if (!substr.includes(letter)) {
+            substr += letter
+        }
+        else {
+            if (substr.length > max) {
+                max = substr.length
+            }
+            substr = substr.slice(substr.indexOf(letter))
+        }
+    }
+    return max
+}
+
+/* WIP - don't actually have time */
