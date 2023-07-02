ffd92d53a91cf399461bf086a2ca50767c8de4ac
Christina Milner <christina@christina-milner.dev>
2023-07-02 12:12:53 +0100

    Updated function composition done

--- 6th-kyu/function-composition.js
+++ 6th-kyu/function-composition.js
@@ -13,3 +13,14 @@ function compose() {
 
   // Love me some recursion
   // Todo: Investigate why reduce didn't work ("a is not a function")
+
+  /* It appears this kata got updated in some way and I did it again without realising I'd already done it until VSCode complained
+  this file already existed.
+  "Compose" now takes two functions specifically as arguments and is to return a function that applies both to an unspecified number of arguments.
+  I wanted to check a "wouldn't that just work like...?" before writing anything down, but that was all there was to it. */
+
+function compose(f,g) {
+    return function(...args) {
+        return f(g(...args))
+    }
+}
