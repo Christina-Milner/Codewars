926599e4766da3c5bd87490028754b0ea1bbc608
Christina Milner <christina@christina-milner.dev>
2023-09-29 21:08:15 +0100

    ones in a segment wip

--- 4th-kyu/count-ones-in-a-segment.js
+++ 4th-kyu/count-ones-in-a-segment.js
@@ -15,17 +15,6 @@
     - Remaining digits are half ones on average?
         - Yeah that checks out, the 4-digit binary numbers come up to 12 1s ignoring the first digit, so 3 / 2 * 8 (the number of them)
 
-        function countOnes(left, right) {
-    for (let i = 0; i <= 10; i++) {
-        let power = 2 ** i
-        let next = 2 ** (i + 1)
-        let bins = []
-        for (let i = power; i < next; i++) {
-            bins.push(i.toString(2))
-        }
-        console.log("From: ", power, "To: ", next, (next - power) + (next - power) * (i / 2), bins.reduce((acc, cur) => acc + cur.split('').filter(e => e == 1).length, 0))
-    }
-}
 - At lower numbers, easy to actually count the ones by doing array reduce. Can confirm that (nextPow - currentPow) + (nextPow - currentPow) * (pow/ 2) gives accurate results for
     currentPow = 2 ** x, nextPow = 2 ** (x + 1), and pow = x
 - So take log 2 of left and right
@@ -35,6 +24,44 @@
 */
 
 
- function countOnes(left, right) {
-  
- }
+function countOnes(left, right) {
+    const lower = Math.ceil(Math.log2(left))
+    const upper = Math.floor(Math.log2(right))
+    let result = 0
+    if (upper > lower) {
+        for (let i = lower; i < upper; i++) {
+            let span = 2 ** (i + 1) - 2 ** i
+            result += span + span * (i / 2)
+        }
+        let missing = []
+        for (let i = left; i < 2 ** lower; i++) {
+            missing.push(i.toString(2))
+        }
+        for (let i = 2 ** upper; i <= right; i++) {
+            missing.push(i.toString(2))
+        }
+        result += missing.reduce((acc, cur) => acc + cur.split('').filter(e => e == 1).length, 0)
+        return result
+    }
+    let missing = []
+    for (let i = left; i <= right; i++) {
+        missing.push(i.toString(2))
+    }
+    return missing.reduce((acc, cur) => acc + cur.split('').filter(e => e == 1).length, 0)
+}
+
+/* Problem is the log2 of left and right is very often only apart by something like 0.3, making most of my thought process moot.
+    Code as it was *sometimes* produced erroneous results and then timed out.
+    With the added if block so it just straight up does iteration if there's no powers of 2 to skip through, it doesn't seem to be
+    producing any incorrect results, but of course it times out before even getting to the huge tests. 
+    Hmm.
+    Left: 878603, right: 955274
+    Result is 843350
+    Left in binary is  11010110100000001011
+    Right in binary is 11101001001110001010
+    The difference between the two is 76671, which is 10010101101111111 in binary
+    The actual number we're looking for is 11001101111001010110 in binary
+    11010110100000001011 (9 1s)
+    11101001001110001010  (10 1s)
+
+    */
