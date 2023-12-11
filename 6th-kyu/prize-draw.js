c1aa2d55a3f71484cb3837a22a675503cadba10f
Christina Milner <christina@christina-milner.dev>
2023-12-11 20:17:25 +0000

    Prize draw done

--- 6th-kyu/prize-draw.js
+++ 6th-kyu/prize-draw.js
@@ -16,7 +16,6 @@ If there are fewer participants than n, return "Not enough participants", if the
 - Return entry at position n - 1 (as ranks start at 1)
 */
 
-
 function rank(str, numArr, n) {
     if (!str.length) {
         return "No participants"
@@ -27,7 +26,7 @@ function rank(str, numArr, n) {
     }
     const sumLetterValues = str => {
         return str.toLowerCase().split('')
-            .map((char, idx) => str.charCodeAt(idx) - 96)
+            .map((char, idx) => str.toLowerCase().charCodeAt(idx) - 96)
             .reduce((acc, cur) => acc + cur, 0)
     }
     let participants = {}
@@ -35,26 +34,13 @@ function rank(str, numArr, n) {
         let person = strArr[i]
         participants[person] = (sumLetterValues(person) + person.length) * numArr[i]
     }
-    console.log("n: ", n)
-    console.log("participants: ", participants)
-    console.log("weights: ", numArr)
-    let sortedParticipants = Object.keys(participants).sort((a, b) => participants[a] == participants[b] ? a.localeCompare(b) : participants[b] - participants[a])
-    console.log("sorted: ", sortedParticipants)
+
+    let sortedParticipants = Object.keys(participants).sort((a, b) => participants[a] == participants[b] ? a.toLowerCase().localeCompare(b.toLowerCase()) : participants[b] - participants[a])
     return sortedParticipants[n - 1]
 }
 
 /* 
-??? Trickier than it looks. Only passes about half the randoms and I'm out of time.
-Initial thought process didn't account for a few things, such as idx not being available for sort() (hence the object solution),
-or the fact that the participants are actually passed in as a string, not an array. 
-n:  2
-participants:  { Willaim: 324, Liam: 28, Daniel: 114, Alexander: 122 }
-weights:  [ 6, 4, 6, 2 ]
-sorted:  [ 'Willaim', 'Alexander', 'Daniel', 'Liam' ]
+So! Solved. Main issue was char code being pulled from str rather than str.toLowerCase(). Oops. Figured that out when I saw that "Elizabeth" was
+getting assigned -27 for the first character.
 
-expected 'Alexander' to equal 'Daniel'
-
-I have no idea why Daniel is meant to come first there unless there's an error in the score calculation, but I feel like too many are passing for that to be the case?
-Locale compare might still need a bit of tweaking as this wants upper and lowercase to be treated the same, but that's not the problem here.
-Anyway, mystery for another day.
 */
